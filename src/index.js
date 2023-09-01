889767f29cfe716d80ee56550513a4ce7913d007
jfpineau <jeanfpineau@gmail.com>
2023-09-01 08:16:09 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import { reaction } from 'mobx';
+
+import createRouter from './utils/router';
+import App from './components/App';
+import './index.css';
+
+import { ShopStore } from './stores/ShopStore';
+
+const fetcher = url => window.fetch(url).then(response => response.json());
+const shop = ShopStore.create(
+  {},
+  {
+    fetch: fetcher,
+    alert: m => window.alert(m),
+  },
+);
+window.shop = shop; // for playing around
+
+ReactDOM.render(
+  <Provider shop={shop}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
+reaction(
+  () => shop.view.currentUrl,
+  (path) => {
+    if (window.location.pathname !== path) {
+      window.history.pushState(null, null, path);
+    }
+  },
+);
+
+const router = createRouter({
+  '/book/:bookId': ({ bookId }) => shop.view.openBookPageById(bookId),
+  '/cart': shop.view.openCartPage,
+  '/': shop.view.openBooksPage,
+});
+
+window.onpopstate = function historyChange(ev) {
+  if (ev.type === 'popstate') router(window.location.pathname);
+};
+
+router(window.location.pathname);
